2c96b48490fd9977759aa1b2eb10d64c4b2b2c02
Nikhil <nikhilv.vislavath@gmail.com>
2026-07-02 10:20:18 +0530

    initil commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st 
+
+st.title('Ticket Tatkal')
+
+col1,col2 = st.columns(2)
+
+with col1:
+    st.image('image.png')
+
+with col2:
+    st.write('You can book Tatkal tickets for a maximum of only 4 passengers per PNR. Book tickets through your IRCTC login ID online on ixigo.' \
+    ' Check Tatkal quota seat availability for your train and ticket confirmation probability/prediction.')
